c174909622f59735b386be1cb1f208843b6a06a2
Vitor Tiradentes <vitortss33@gmail.com>
2024-09-19 22:01:52 -0300

    sparkframework

--- spark.demo/src/main/java/app/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import static spark.Spark.*;
-
-public class HelloWorld {
-		public static void main(String args[]) {
-			get("/hello", (request, response) -> "Hello World!");
-		}
-}
